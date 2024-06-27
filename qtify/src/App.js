536cbfc04929d3f9273f054f4d6b4f45774f3b0a
Kartik <19kartikgupta@gmail.com>
2024-06-28 05:29:27 +0530

    Final Qtify push

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,39 +1,119 @@
-import React from 'react';
-import './App.css';
-// import { useEffect, useState } from 'react';
-// import {fetchNewAlbums, fetchSongs, fetchTopAlbums} from "./api/Api";
-import StyledEngineProvider from "@mui/material/StyledEngineProvider";
-import Navbar from "./components/Navbar/Navbar";
-// import { Outlet } from "react-router-dom";
-import Hero from './components/Hero/Hero';
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Hero from './components/Hero/Hero.jsx'
+import styles from './App.module.css'
+import { useEffect, useState } from 'react'
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from './api/api'
+import Section from './components/Section/Section.jsx'
+import FilterSection from './components/FilterSection/FilterSection'
 
 function App() {
-  // const [data, setdata] = useState({});
-  // const generateData = (key, source) => {
-  //   source().then((data) => {
-  //     setdata((prevState) => {
-  //       return { ...prevState, [key]: data };
-  //     });
-  //   });
-  // };
 
-  // useEffect(() => {
-  //   generateData("topAlbums", fetchTopAlbums);
-  //   generateData("newAlbums", fetchNewAlbums);
-  //   generateData("songs", fetchSongs);
-  // }, []);
+  const [topAlbumSongs, setTopAlbumSongs] = useState([])
+  const [newAlbumSongs, setNewAlbumSongs] = useState([])
+  
+  const [filteredDataValues, setFilteredDataValues] = useState([''])
+  const [toggle, setToggle] = useState(false)
+  const [value, setValue] = useState(0);
 
-  // const { topAlbums = [], newAlbums = [], songs = [] } = data;
+  const generateSongsData = (value) => {
+    let songData = newAlbumSongs[0].songs;
+    let key;
+    if (value === 0) {
+      setFilteredDataValues(songData)
+      return;
+    }
+    else if (value === 1) {
+      key = 'rock'
+    }
+    else if (value === 2) {
+      key = 'pop'
+    }
+    else if (value === 3) {
+      key = 'jazz'
+    }
+    else if (value === 4) {
+      key = 'blues'
+    }
+    const data = songData.filter((item) => {
+      return item.genre.key === key
+    })
+    setFilteredDataValues(data)
+  }
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+    generateSongsData(newValue)
+  }
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
+  const filteredData = (val) => {
+    generateSongsData(val)
+    // console.log(val + ' filteredData is called from app.js')
+  }
+
+  const generateTopAlbumSongs = async () => {
+    try {
+      const topAlbumSongs = await fetchTopAlbums()
+      setTopAlbumSongs(topAlbumSongs)
+    }
+    catch (error) {
+      console.log(error)
+      return null
+    }
+
+  }
+  const generateNewAlbumSongs = async () => {
+    try {
+      const newAlbumSongs = await fetchNewAlbums()
+      setNewAlbumSongs(newAlbumSongs);
+      // setFilteredDataValues(newAlbumSongs);
+    }
+    catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  const generateFilterSongs = async () => {
+    try {
+      const newAlbumSongs = await fetchSongs()
+      // setNewAlbumSongs(newAlbumSongs);
+      setFilteredDataValues(newAlbumSongs);
+    }
+    catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line
+  }, [value])
+
+  useEffect(() => {
+    
+    generateTopAlbumSongs();
+    generateNewAlbumSongs();
+    generateFilterSongs();
+    // setFilteredDataValues(newAlbumSongs);
+
+  }, [])
+  
+  
   return (
     <>
-      <StyledEngineProvider injectFirst>
-        <Navbar  />  
-        <Hero/>
-        {/* searchData={[...topAlbums, ...newAlbums]} */}
-        {/* <Outlet context={{ data: { topAlbums, newAlbums, songs } }} /> */}
-      </StyledEngineProvider>
+      <Navbar />
+      <Hero />
+      <div className={styles.sectionWrapper}>
+        <Section type='album' title='Top Albums' data={topAlbumSongs} />
+        <Section type='album' title='New Albums' data={newAlbumSongs} />
+        <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
+      </div>
     </>
-  );
+  )
 }
-export default App;
+
+export default App
--- qtify/src/index.js
+++ qtify/src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css';
 import App from './App';
-// import HomePage from "./pages/AlbumPage/AlbumPage";
-// import AlbumPage from "./pages/HomePage/HomePage";
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App />,
-    Children: [
-      {
-      path: "/",
-      // element: <HomePage />,
-      },
-      {
-        path: "/album/:albumId",
-        // element: <AlbumPage />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-
